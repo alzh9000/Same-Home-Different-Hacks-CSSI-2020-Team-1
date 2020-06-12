a6530f9ce99512034354dbb3a24ded94c8d58861
yuyuan luo <luo.yuyuan@yahoo.com>
2020-06-12 17:05:47 -0400

    get rid of vid

--- js/video.js
+++ js/video.js
@@ -15,21 +15,23 @@ var start, end, vid;
 
 $(document).ready(function () {
     var timestamps = ['00:03', '00:07', '00:15', '01:22']
-    vid = $('#vid')[0];
+    var d = 10000;
     for (var i = 0; i < timestamps.length; i++) {
         timestamps[i] = stamp2sec(timestamps[i]);
     }
-    var starti = -1;
-    var endi = 0;
-    start = 0;
-    if (timestamps.length > 0) end = timestamps[0];
-    else end = vid.duration;
 
     var video = videojs("vid", {
         plugins: {
             abLoopPlugin: {}
         }
     });
+
+    var starti = -1;
+    var endi = 0;
+    start = 0;
+    if (timestamps.length > 0) end = timestamps[0];
+    else end = d;
+    console.log(d);
 
     $("#next").click(function () {
         if (endi < timestamps.length - 1) {
@@ -41,7 +43,7 @@ $(document).ready(function () {
             starti += 1;
             endi += 1;
             start = end;
-            end = vid.duration;
+            end = d;
         }
         video.ready(function () {
             this.abLoopPlugin.setStart(start).setEnd(end).playLoop();
@@ -69,7 +71,7 @@ $(document).ready(function () {
     $("#rec").click(function () {
         record = true;
         video.ready(function () {
-            this.abLoopPlugin.setStart(0).setEnd(vid.duration).togglePauseAfterLooping().playLoop();
+            this.abLoopPlugin.setStart(0).setEnd(d).togglePauseAfterLooping().playLoop();
         });
     });
 
