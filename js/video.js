3a7dba8d5dcc4194e03398019ae3131828266a1c
yuyuan luo <luo.yuyuan@yahoo.com>
2020-06-12 14:52:59 -0400

    bug fix for buttons

--- js/video.js
+++ js/video.js
@@ -78,12 +78,12 @@ $(document).ready(function () {
     });
 
     //WEBCAM
-    var video = document.querySelector("#videoElement");
+    var wc = document.querySelector("#videoElement");
 
     if (navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true })
             .then(function (stream) {
-            video.srcObject = stream;
+            wc.srcObject = stream;
             })
             .catch(function (err0r) {
             console.log("Something went wrong!");
