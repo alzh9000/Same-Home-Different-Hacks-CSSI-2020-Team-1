634703c3c3d58e713cd75ac0a1801f67d70d6a70
Tiffany <cnnmonn@gmail.com>
2020-06-12 23:39:08 -0400

    connecting user data

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
   initChart();
+  initProfile();
 });
+
+let red = 'rgb(231, 111, 081)';
+let orange = 'rgb(244, 162, 097)';
+let yellow = 'rgb(233, 196, 106)';
+let green = 'rgb(042, 157, 143)';
+let black = 'rgb(038, 070, 083)';
+
+function initProfile() {
+  let myName = localStorage.getItem("name") || 'Name';
+  let myID = localStorage.getItem("uid") || 'ID';
+
+  $("#name").html(myName);
+  $("#uid").html("user id: " + myID);
+}
 
 function initChart() {
   Chart.defaults.global.defaultFontSize = 16
@@ -9,26 +24,29 @@ function initChart() {
   var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+        labels: ["06/04/19", "06/05/19", "06/06/19", "06/07/19", "06/08/19", "06/09/19", "06/10/19", "06/11/19", "06/12/19"],
         datasets: [{
-          label: 'Rick Astley - Never Gonna Give You Up (Video)',
-          backgroundColor: "#caf270",
-          data: [12, 59, 5, 56, 58, 12, 59, 87, 45],
+          label: 'Rick Astley - Never Gonna Give You Up',
+          backgroundColor: red,
+          data: [98, 37, 0, 0, 0, 0, 0, 0, 0],
         }, {
           label: 'Darude - Sandstorm',
-          backgroundColor: "#45c490",
-          data: [12, 59, 5, 56, 58, 12, 59, 85, 23],
+          backgroundColor: orange,
+          data: [30, 70, 75, 90, 0, 0, 0, 0, 0],
         }, {
           label: 'All Star - Smash Mouth',
-          backgroundColor: "#008d93",
-          data: [12, 59, 5, 56, 58, 12, 59, 65, 51],
+          backgroundColor: yellow,
+          data: [0, 0, 0, 0, 58, 80, 87, 0, 51],
         }, {
           label: 'Big Time Rush - Big Time Rush',
-          backgroundColor: "#2e5468",
-          data: [12, 59, 5, 56, 58, 12, 59, 12, 74],
+          backgroundColor: green,
+          data: [0, 0, 0, 0, 0, 0, 0, 12, 74],
         }]
       },
       options: {
+        legend: {
+          display: false
+        },
         tooltips: {
           displayColors: true,
           callbacks: {
