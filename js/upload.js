a4a781dba9d140600f98f546f13c05986918a5a1
Tiffany <cnnmonn@gmail.com>
2020-06-13 14:26:24 -0400

    more upload stuff & small fixes
    renamed a bunch of titles so they're consistent

--- js/upload.js
+++ js/upload.js
@@ -1,3 +1,17 @@
+$(document).ready(function() {
+  $('#file').change(function(e){
+    $('#file-label').html(getfileName(e.currentTarget.value));
+  });
+});
+
+function getfileName(fullPath){
+  let startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+  let fileName = fullPath.substring(startIndex);
+  if (fileName.indexOf('\\') === 0 || fileName.indexOf('/') === 0) fileName = fileName.substring(1);
+  if (fileName === '') fileName = "No file selected.";
+  return fileName;
+}
+
 function goHome() {
   window.location.href = "index.html";
 }
