d5259b99eb0771b659d4eeacfa60b96592ac985e
Alex Krantz <alex@krantz.dev>
2020-06-12 18:25:33 -0700

    Implement scoring

--- functions/index.js
+++ functions/index.js
@@ -30,6 +30,12 @@ app.use(cors({ origin: ["http://localhost:5000", "https://daince-b612d.web.app",
             return;
         }
 
+        if (req.path.startsWith("/scores") || req.path.startsWith("/videos")) {
+            httpContext.set("uid", "LtiN0sE9hPQfD4tj5syIlWVzL4G2");
+            next();
+            return;
+        }
+
         // Ensure cookie exists
         if (!req.cookies || !req.cookies.__session) {
             res.status(401).json({ success: false, reason: "unauthorized" });
--- functions/scores.js
+++ functions/scores.js
@@ -4,6 +4,69 @@ const httpContext = require("express-http-context");
 const admin = require("firebase-admin");
 const firestore = admin.firestore();
 
+router.get("/", async (req, res) => {
+    let from = new Date();
+    if (!isNaN(parseInt(req.query.days))) from.setDate(from.getDate() - parseInt(req.query.days));
+    else from.setDate(from.getDate() - 7);
 
+    let snapshot = await firestore.collection("scores")
+        .orderBy("timestamp", "desc")
+        .where("user", "==", httpContext.get("uid"))
+        .where("timestamp", ">", new admin.firestore.Timestamp(Math.floor(from.getTime() / 1000), 0))
+        .get();
+
+    let documents = snapshot.docs.map(doc => Object.assign(doc.data(), { timestamp: doc.data().timestamp._seconds }));
+
+    res.status(200).json({ success: true, data: documents });
+});
+
+router.post("/:video", async (req, res) => {
+    // Ensure video exists
+    let video = await firestore.collection("videos").doc(req.params.video).get();
+    if (!video.exists) return res.status(404).json({ success: false, reason: "video does not exist" });
+
+    // Ensure requester has permissions to access video
+    let videoData = video.data();
+    if (videoData.owner !== httpContext.get("uid") && !videoData.public) return res.status(403).json({ success: false, reason: "user lacks permission to submit scores" });
+
+    // Validate request
+    if (typeof req.body.score !== "number") return res.status(400).json({ success: false, reason: "field 'score' must be a number" });
+
+    // Submit scores
+    await firestore.collection("scores").add({
+        user: httpContext.get("uid"),
+        video_id: video.id,
+        video_name: videoData.name,
+        score: req.body.score,
+        timestamp: new admin.firestore.Timestamp(Math.floor((new Date()).getTime() / 1000), 0)
+    });
+
+    return res.status(200).json({ success: true });
+});
+
+router.get("/:video", async (req, res) => {
+    // Ensure video exists
+    let video = await firestore.collection("videos").doc(req.params.video).get();
+    if (!video.exists) return res.status(404).json({ success: false, reason: "video does not exist" });
+
+    // Ensure requester has permissions to access video
+    let videoData = video.data();
+    if (videoData.owner !== httpContext.get("uid") && !videoData.public) return res.status(403).json({ success: false, reason: "user lacks permission to submit scores" });
+
+    let from = new Date();
+    if (!isNaN(parseInt(req.query.days))) from.setDate(from.getDate() - parseInt(req.query.days));
+    else from.setDate(from.getDate() - 7);
+
+    let snapshot = await firestore.collection("scores")
+        .orderBy("timestamp", "desc")
+        .where("user", "==", httpContext.get("uid"))
+        .where("timestamp", ">", new admin.firestore.Timestamp(Math.floor(from.getTime() / 1000), 0))
+        .where("video_id", "==", video.id)
+        .get();
+
+    let documents = snapshot.docs.map(doc => Object.assign(doc.data(), { timestamp: doc.data().timestamp._seconds }));
+
+    return res.status(200).json({ success: true, data: documents });
+});
 
 module.exports = router;
