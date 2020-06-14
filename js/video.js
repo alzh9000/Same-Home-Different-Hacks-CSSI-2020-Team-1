ada7eae2ffc0bb2f23b69203286b789372486d44
kyang8093 <kgyang@stanford.edu>
2020-06-13 21:34:39 -0600

    Co-authored-by: yuyuan-luo <luo.yuyuan@yahoo.com> Co-authored-by: Albert Zhang <albertzhang9000@gmail.com> Co-authored-by: cnnmon <cnnmonn@gmail.com> Co-authored-by: devonnbusoy@yahoo.com <devonnbusoy@yahoo.com>

--- js/video.js
+++ js/video.js
@@ -14,12 +14,30 @@ async function loadPosenet() {
 
 
 //video player
-var start, end, video, poses, fileread;
+var start, end, video, poses, fileread, record, sumscores, sumframes;
 var timestamps = ['00:03', '00:07', '00:15', '01:22']
 var endi = 0;
 
+var color;
+var canvas;
+var ctx;
 
 $(document).ready(function () {
+    color = 'aqua';
+    canvas = document.getElementById('canvas');
+    canvas.style.zIndex = 3;
+
+    // canvas = document.getElementById('videoElement');
+    console.log(canvas.getContext);
+    ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
+
+
     loadPosenet();
 
     var d = 10000;
@@ -74,9 +92,11 @@ $(document).ready(function () {
         });
     });
 
-    var record = false;
+    record = false;
     $("#rec").click(function () {
         record = true;
+        sumscores = 0;
+        sumframes = 0;
         video.ready(function () {
             this.abLoopPlugin.setStart(0).setEnd(d).togglePauseAfterLooping().playLoop();
         });
@@ -98,12 +118,12 @@ $(document).ready(function () {
         console.log(poses);
         fileread = true;
     }
-    
+
     check();
-    
+
 });
 
-var check = function(){
+var check = function () {
     if (fileread === true) {
         Webcam.set({
             width: 640,
@@ -116,9 +136,8 @@ var check = function(){
         Webcam.on('live', function () {
             take_snapshot();
         });
-    } 
-    else {
-        setTimeout(check, 1000); 
+    } else {
+        setTimeout(check, 1000);
     }
 }
 
@@ -238,14 +257,42 @@ function take_snapshot() {
     setTimeout(take_snapshot, 1000);
 }
 
+var result;
+
+/**
+ * DRAWING FUNCTIONS
+ */
+
+function drawPoint(y, x, r, color) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+function drawKeypoints(keypoints, scale = 1) {
+    for (let i = 0; i < keypoints.length; i++) {
+        const keypoint = keypoints[i];
+        const {
+            y,
+            x
+        } = keypoint.position;
+        drawPoint(y * scale, x * scale, 3, color);
+    }
+}
+
+
 function posenetImg(inputimg) {
     posenet.load().then(function (net) {
         net.estimateSinglePose(inputimg, {
             flipHorizontal: true
         }).then(function (pose) {
-            // console.log(pose);
-            // console.log(poses[Math.round(video.currentTime() / 0.2)]);
-            var result = compPoseNet(pose, poses[Math.round(video.currentTime() / 0.2)]);
+            //draw the keypoints
+            console.log(pose["keypoints"]);
+            drawKeypoints(pose["keypoints"]);
+
+            console.log(poses[Math.round(video.currentTime() / 0.2)]);
+            result = compPoseNet(pose, poses[Math.round(video.currentTime() / 0.2)]);
             //console.log(result);
             document.getElementById("score").innerHTML = result;
         });
@@ -265,109 +312,109 @@ function compPoseNet(poseNet1, poseNet2) {
     score = weightedDistanceMatching(vec1, vec2);
     // console.log(score)
     return score
-  }
-  
-  function vectorizePoseNet(poseNet1) {
+}
+
+function vectorizePoseNet(poseNet1) {
     // Initialize the PoseNet vector as an empty arrays that we'll add to
     let vec1 = new Array();
-  
+
     for (keypoint in poseNet1["keypoints"]) {
-      let coordinates = poseNet1["keypoints"][keypoint]["position"];
-      // console.log(coordinates);
-      let xCoor = coordinates['x'];
-      let yCoor = coordinates['y'];
-      vec1.push(xCoor);
-      vec1.push(yCoor);
-      // console.log(vec1);
+        let coordinates = poseNet1["keypoints"][keypoint]["position"];
+        // console.log(coordinates);
+        let xCoor = coordinates['x'];
+        let yCoor = coordinates['y'];
+        vec1.push(xCoor);
+        vec1.push(yCoor);
+        // console.log(vec1);
     }
-  
+
     // vec1 = array1;
     // console.log(vec1)
-  
+
     // At this moment, the vec1 is not scaled or normalized. This code will do that before combining it with the scores. :) 
     // To scale the vectors, we subtract the minimum coordinate value of that axis from all the coordinates for that axis
     minX = Number.MAX_VALUE
     minY = Number.MAX_VALUE
     for (i = 0; i < 33; i += 2) {
-      // console.log(minX)
-      currentX = vec1[i];
-      // console.log(currentX)
-      if (currentX < minX) {
-        minX = currentX;
-      }
-      // console.log(i);
+        // console.log(minX)
+        currentX = vec1[i];
+        // console.log(currentX)
+        if (currentX < minX) {
+            minX = currentX;
+        }
+        // console.log(i);
     }
     for (i = 1; i < 34; i += 2) {
-      // console.log(minY)
-      currentY = vec1[i];
-      // console.log(currentY)
-      if (currentY < minY) {
-        minY = currentY;
-      }
-      // console.log(i);
+        // console.log(minY)
+        currentY = vec1[i];
+        // console.log(currentY)
+        if (currentY < minY) {
+            minY = currentY;
+        }
+        // console.log(i);
     }
     for (i = 0; i < 33; i += 2) {
-      vec1[i] -= minX;
+        vec1[i] -= minX;
     }
     for (i = 1; i < 34; i += 2) {
-      vec1[i] -= minY;
+        vec1[i] -= minY;
     }
-  
+
     // console.log(vec1)
-  
+
     // Conduct L2 Vector Normalization
-  
+
     let squaredSum = 0
     for (i = 0; i < 34; i++) {
-      squaredSum += Math.pow(vec1[i], 2)
+        squaredSum += Math.pow(vec1[i], 2)
     }
     // console.log(squaredSum)
-  
+
     let normalizedCoefficient = Math.sqrt(squaredSum)
     // console.log(normalizedCoefficient)
-  
+
     for (i = 0; i < 34; i++) {
-      vec1[i] /= normalizedCoefficient
+        vec1[i] /= normalizedCoefficient
     }
-  
+
     // console.log(vec1)
-  
-  
+
+
     // Finish by adding the other values needed in the vector
     let total_confidence1 = 0;
     for (keypoint in poseNet1["keypoints"]) {
-      let confidence = poseNet1["keypoints"][keypoint]["score"];
-      // console.log(total_confidence1);
-      vec1.push(confidence);
-      total_confidence1 += confidence;
+        let confidence = poseNet1["keypoints"][keypoint]["score"];
+        // console.log(total_confidence1);
+        vec1.push(confidence);
+        total_confidence1 += confidence;
     }
     vec1.push(total_confidence1);
     return vec1;
-  }
-  
-  // poseVector1 and poseVector2 are 52-float vectors composed of:
-  // Values 0-33: are x,y coordinates for 17 body parts in alphabetical order
-  // Values 34-51: are confidence values for each of the 17 body parts in alphabetical order
-  // Value 51: A sum of all the confidence values
-  // Again the lower the number, the closer the distance
-  function weightedDistanceMatching(poseVector1, poseVector2) {
+}
+
+// poseVector1 and poseVector2 are 52-float vectors composed of:
+// Values 0-33: are x,y coordinates for 17 body parts in alphabetical order
+// Values 34-51: are confidence values for each of the 17 body parts in alphabetical order
+// Value 51: A sum of all the confidence values
+// Again the lower the number, the closer the distance
+function weightedDistanceMatching(poseVector1, poseVector2) {
     let vector1PoseXY = poseVector1.slice(0, 34);
     let vector1Confidences = poseVector1.slice(34, 51);
     let vector1ConfidenceSum = poseVector1.slice(51, 52);
-  
+
     let vector2PoseXY = poseVector2.slice(0, 34);
-  
+
     // First summation
     let summation1 = 1 / vector1ConfidenceSum;
-  
+
     // Second summation
     let summation2 = 0;
     for (let i = 0; i < vector1PoseXY.length; i++) {
-      let tempConf = Math.floor(i / 2);
-      let tempSum = vector1Confidences[tempConf] * Math.abs(vector1PoseXY[i] - vector2PoseXY[i]);
-      summation2 = summation2 + tempSum;
+        let tempConf = Math.floor(i / 2);
+        let tempSum = vector1Confidences[tempConf] * Math.abs(vector1PoseXY[i] - vector2PoseXY[i]);
+        summation2 = summation2 + tempSum;
     }
     // console.log(summation1)
     // console.log(summation2)
     return 1 - summation1 * summation2;
-  }
+}
